626ed3470fbc41224bd3792d04d6bba444d0d911
smminassian <sebastienminassian203@gmail.com>
2024-12-01 17:29:27 -0800

    working on combining wc -l into one  array cell  block instead of 2

--- code.c
+++ code.c
@@ -4,17 +4,20 @@
 #include <string.h>
 
 int main(int argc, char* argv[]){
-     int in_fd;
-     int out_fd;
-    if(argc == 3){
-        char *inp = argv[1];
-        char *cmd = argv[2];
-        char *out = argv[3];
+     if(argc == 4){
+        char**newargv = (char**)malloc(sizeof(char*) * argc-1);
 
-        
-
-        if(strcmp(inp, "-") != 0){
-            in_fd = open(inp, O_RDonly);
+        for(int ix = 1; ix < argc; ix++){
+            newargv[ix-1] = (char*)argv[ix];
         }
-    }
+        newargv[argc-1] = NULL;
+
+        printf("arr[2]:  %s\n", argv[2]);
+        printf("arr[3]: %s\n", argv[3]);
+
+        //Need to make argv[1] and argv[2] be one output. So this means that i need to split the space in between the argv2 and argv3 and the push that 
+        //split to newargv[1]
+        free(newargv);
+     }
+     return 0;
 }
