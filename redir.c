3dd26634e9b0699153f55824180f2c6e9d951419
smminassian <sebastienminassian203@gmail.com>
2024-12-06 16:06:21 -0800

    taking a break

--- redir.c
+++ redir.c
@@ -5,33 +5,41 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <stdbool.h>
 
 int main(int argc, char* argv[]){
-     if(argc == 3){
+     if(argc < 3){
         fprintf(stderr, "Usage: %s <outputFile> command [<arg1> <arg2> ...]\n", argv[0]);
+        exit(1);
      }
-     int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-     if (fd == -1) {
-        fprintf(stderr, "Failed to open %s\n", argv[1]);
-        return 1;
-     }
-
      char** newargv = (char**)malloc(sizeof(char*) *  (1 + argc - 2));
+      bool redirect_output_to_a_file = false;
+      int fd;
+     if(strcmp(argv[1], "-") != 0){
+      redirect_output_to_a_file = true;
+      fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+     }
 
-     for (int ix = 2; ix < argc; ix++){
-        newargv[ix-2] = (char*)argv[ix];
+
+      for (int ix = 2; ix < argc; ix++){
+         printf("Copying %s to newargv[%d]\n", argv[ix], ix-2);
+         newargv[ix-2] = (char*)argv[ix];
      }
 
      int child_pid = fork();
      if (child_pid == 0){
+      if(redirect_output_to_a_file){
         dup2(fd, STDOUT_FILENO);
         close(fd);
+      }
 
         execve(newargv[0], newargv, NULL);
      }
-
-     wait(NULL);
+      wait(NULL);
      printf("%s pid is %d. forked %d. Parent Exiting\n", argv[0], getpid(), child_pid);
-
-     return 0;
+     
+    
+      
+      free(newargv);
+      return 0;
 }
