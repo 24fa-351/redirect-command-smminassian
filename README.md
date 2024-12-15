e577c47494106a2fe9fe7c89a703d7501b910237
smminassian <sebastienminassian203@gmail.com>
2024-12-15 13:46:45 -0800

    so program works to a degree. Some commands work. Read my readme to get more info

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# 351-redirect
-empty for project
+So the format is cmd <inputfile> <outputfile>
+
+
+It was very hard to do <inputfile> cmd <outputfile> because I just didnt know how to get the input from the inputfile and pass it.
+
+So i decided to put the format as cmd <inputfile> <outputfile>
+
+
+
+So the commands that work are cat. If you do /usr/bin/cat inputfile outputfile, it will work. Now if you do /usr/bin/wc <inputfile> <outputfile>, it will work for the inputfile but it also displays the output file.
+
+If you try to do -l, it will redirect to a file called -l so dont do that.
--- redir.c
+++ redir.c
@@ -6,49 +6,25 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <unistd.h>
-//void split(char **newargv, char **words[], char delimiter);
-
-
-
-// void split(char **newargv, char **words[], char delimiter)
-// {
-//     int word_count = 0;
-//     char **next_char = newargv;
-//     char current_word[1000];
-//     strcpy(current_word, "");
-
-//     while (**next_char != '\0')
-//     {
-//         if (**next_char == delimiter)
-//         {
-//             words[word_count++] = strdup(current_word);
-//             strcpy(current_word, "");
-//         }
-//         ++next_char;
-//     }
-//     words[word_count++] = strdup(current_word);
-//     words[word_count] = NULL;
-// }
-
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
         fprintf(
             stderr,
-            "Usage: %s <inputFile> command <outputFile> [<arg1> <arg2> ...]\n",
+            "Usage: %s command <inputFile> <outputFile> [<arg1> <arg2> ...]\n",
             argv[0]);
         exit(1);
     }
     char** newargv = (char**)malloc(sizeof(char*) * (1 + argc - 2));
-    //char words[argc];
-    //split(newargv, words, " ");
+
+    
     bool redirect_output_to_a_file = false;
     bool redirect_input_to_a_file = false;
 
     int input_fd;
-    if (strcmp(argv[1], "-") != 0) {
+    if (strcmp(argv[2], "-") != 0) {
         redirect_input_to_a_file = true;
-        input_fd = open(argv[1], O_WRONLY | S_IRUSR | S_IWUSR);
+        input_fd = open(argv[1], O_RDONLY);
         if (input_fd == -1)
             perror("File failed to open");
     }
@@ -63,22 +39,32 @@ int main(int argc, char* argv[]) {
     }
 
     for (int ix = 2; ix < argc; ix++) {
-        printf("Copying %s to newargv[%d]\n", argv[ix], ix - 2);
-        newargv[ix - 2] = (char*)argv[ix];
+        printf("Copying %s to newargv[%d]\n", argv[ix], ix - 1);
+        newargv[ix - 1] = (char*)argv[ix];
     }
+    newargv[argc - 1] = NULL;
+
+    newargv[0] = argv[1];
 
     int child_pid = fork();
     if (child_pid == 0) {
         if (redirect_input_to_a_file) {
-            dup2(input_fd, STDIN_FILENO);
-            close(input_fd);
+            if (dup2(input_fd, STDIN_FILENO) == -1) {
+                perror("Failed to redirect input");
+                close(input_fd);
+            }
         }
         if (redirect_output_to_a_file) {
-            dup2(output_fd, STDOUT_FILENO);
-            close(output_fd);
+            if (dup2(output_fd, STDOUT_FILENO) == -1) {
+                perror("Failed to redirect output");
+                close(output_fd);
+            }
         }
 
-        execve(newargv[0], newargv, NULL);
+        int val = execve(newargv[0], newargv, NULL);
+        if(val == -1){
+            perror("Execve failed");
+        }
     }
     wait(NULL);
     printf("%s pid is %d. forked %d. Parent Exiting\n", argv[0], getpid(),
